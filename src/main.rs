903311eac2c012d8f1188b96c5fc066535f2ee9f
Andrew Consroe <aconz2@gmail.com>
2026-02-06 18:19:10 -0600

    okay tried with whole canvas
    probably need sub

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#![allow(unused)]
+
 use std::fs::File;
 use std::io::Read;
-use image::{DynamicImage, Rgba, Rgb, Pixel, Luma};
+use image::{DynamicImage, Rgba, Rgb, Pixel, Luma, GrayImage};
 
 // so it looks like 0.42 kerning
 
@@ -70,37 +72,15 @@ const FONT_FILE: &str = "Courier New.ttf";
 
 use font_kit::canvas::{Canvas, RasterizationOptions, Format};
 use font_kit::hinting::HintingOptions;
-use font_kit::loaders::freetype;
+use font_kit::loaders::freetype::Font;
 use pathfinder_geometry::transform2d::Transform2F;
-use pathfinder_geometry::vector::Vector2F;
-use pathfinder_geometry::rect::RectF;
+use pathfinder_geometry::vector::{Vector2F,Vector2I};
+use pathfinder_geometry::rect::{RectI,RectF};
 
-fn test_image() {
-    let size = 13.0;
-    //let start_pos = Vector2F::new(45.0, 48.);
-    let kern_x = 1.125;
-    let line_height = 1.18;
-    //let start_pos = Vector2F::new(45.0, 48. + size * line_height);
-    //let start_pos = Vector2F::new(45.0, 63.);
-    let start_pos = Vector2F::new(0., 0.);
-    // line height is 15.0 points
-
+fn render(font: &Font, text: &str, size: f32, kern_x: f32, start_pos: Vector2F) -> Canvas {
     let format = Format::A8;
     let rasterization = RasterizationOptions::GrayscaleAa;
-    let transform = Transform2F::default();
     let hinting = HintingOptions::Full(size);
-
-    let font = freetype::Font::from_path("Courier New.otf", 0).unwrap();
-    let metrics = font.metrics();
-    let height = metrics.ascent - metrics.descent;
-    println!("height {height} {}", height as f32 / 24. / 96.);
-
-    println!("metrics {:?}", font.metrics());
-    println!("mono? {}", font.is_monospace());
-
-    //let text = "> MTcxODExL04";
-    let text = "> MTcxODExL04gMi9UIDI3NTY3OC9IIFsgNTIyIDIzMV0+Pg1lbmRvYmoNICAgICAgICAgICAgICAg";
-    //let text = "> DQo1";
 
     let mut glyph_pos = vec![];
     glyph_pos.reserve(text.len());
@@ -120,7 +100,7 @@ fn test_image() {
             .raster_bounds(
                 *glyph_id,
                 size,
-                transform.translate(*pos),
+                Transform2F::from_translation(*pos),
                 hinting,
                 rasterization,
             )
@@ -128,7 +108,7 @@ fn test_image() {
         bounds = bounds.union_rect(raster_rect.to_f32());
     }
 
-    println!("{:?}", bounds);
+    println!("bounds={:?}", bounds);
 
     let mut canvas = Canvas::new(bounds.round().to_i32().size(), format);
 
@@ -143,10 +123,97 @@ fn test_image() {
         )
         .unwrap();
     }
+    canvas
+}
 
+fn decode_line(reference: &GrayImage, font: &Font, nchars: usize, size: f32, kern_x: f32, start_pos: Vector2F, alphabet: &str) -> String {
+    let format = Format::A8;
+    let rasterization = RasterizationOptions::GrayscaleAa;
+    let transform = Transform2F::default();
+    let hinting = HintingOptions::Full(size);
+
+    let (w, h) = reference.dimensions();
+    let mut canvas = Canvas::new(Vector2I::new(w as i32, h as i32), format);
+    println!("decode canvas size {:?}", canvas.size);
+
+    let mut pos = start_pos;
+    println!("(pos {pos:?})");
+
+    let char_gids: Vec<_> = alphabet.chars().map(|c| (c, font.glyph_for_char(c).unwrap())).collect();
+    println!("{char_gids:?}");
+
+    let mut score_glyph = |(_char, gid): &&(char, u32)| {
+        canvas.pixels.fill(0);
+
+        let raster_rect = font
+            .raster_bounds(
+                *gid,
+                size,
+                Transform2F::from_translation(pos),
+                hinting,
+                rasterization,
+            )
+            .unwrap();
+        //println!("raster rect {raster_rect:?}");
+        font.rasterize_glyph(
+            &mut canvas,
+            *gid,
+            size,
+            // have to use the origin from as if we had a whole line, not just a single char...
+            Transform2F::from_translation(Vector2F::new(0., 9.)).translate(pos),
+            hinting,
+            rasterization,
+        )
+        .unwrap();
+
+        let mut pixels = canvas.pixels.clone();
+        for px in pixels.iter_mut() {
+            *px = 255 - *px;
+        }
+        GrayImage::from_raw(w, h, pixels).unwrap().save("foo.png").unwrap();
+
+        let sqr = |x| { x * x };
+
+        let diff: i64 = reference
+            .as_raw()
+            .iter()
+            .zip(canvas.pixels.iter())
+            .map(|(reference, canvas)| sqr(*reference as i32 - ((255 - canvas) as i32)) as i64)
+            .sum();
+        diff
+    };
+
+    (0..nchars)
+        .map(|_| {
+            let (c, _) = char_gids.iter().min_by_key(&mut score_glyph).unwrap();
+            c
+        })
+        .collect()
+}
+
+fn test_image() {
+    let size = 13.0;
+    let kern_x = 1.125;
+    let line_height = 15.; // amount to skip down to write next line
+    //let start_pos = Vector2F::new(45.0, 48. + 15.); // < this matches the start pos of the whole page
+    let start_pos = Vector2F::new(0., 0.);
+
+    let font = Font::from_path("Courier New.otf", 0).unwrap();
+    let metrics = font.metrics();
+    let height = metrics.ascent - metrics.descent;
+    println!("height {height} {}", height as f32 / 24. / 96.);
+
+    println!("metrics {:?}", font.metrics());
+    println!("mono? {}", font.is_monospace());
+
+    let text = "> MTcxODExL04gMi9UIDI3NTY3OC9IIFsgNTIyIDIzMV0+Pg1lbmRvYmoNICAgICAgICAgICAgICAg";
+    //let text = "> DQo1MTcxODExL04gMi9UIDI3NTY3OC9IIFsgNTIyIDIzMV0-----------------------------";
+    //let text = ">";
+
+    let canvas = render(&font, text, size, kern_x, start_pos);
     let w = canvas.size.x() as usize;
     let h = canvas.size.y() as usize;
-    println!("w={w} h={h}");
+    println!("render canvas size w={w} h={h}");
 
     let mut image = DynamicImage::new_rgba8(w as u32, h as u32).to_rgba8();
     for y in 0..h {
@@ -162,31 +229,51 @@ fn test_image() {
     }
     image.save("font_kit.png").unwrap();
 
-    let mut image = DynamicImage::new_luma8(w as u32, h as u32).to_luma8();
-    for px in image.pixels_mut() {
-        *px = Luma([255]);
+    //let mut image = DynamicImage::new_luma8(w as u32, h as u32).to_luma8();
+    //for px in image.pixels_mut() {
+    //    *px = Luma([255]);
+    //}
+    //for y in 0..h {
+    //    let (row_start, row_end) = (y * canvas.stride, (y + 1) * canvas.stride);
+    //    let row = &canvas.pixels[row_start..row_end];
+    //    for x in 0..w {
+    //        let c = 255 - row[x];
+    //        if c != 255 {
+    //            let color = Luma([c]);
+    //            image.put_pixel(x as u32, y as u32, color);
+    //        }
+    //    }
+    //}
+    let mut pixels = canvas.pixels.clone();
+    for px in pixels.iter_mut() {
+        *px = 255 - *px;
     }
-    for y in 0..h {
-        let (row_start, row_end) = (y * canvas.stride, (y + 1) * canvas.stride);
-        let row = &canvas.pixels[row_start..row_end];
-        for x in 0..w {
-            let c = 255 - row[x];
-            if c != 255 {
-                let color = Luma([c]);
-                image.put_pixel(x as u32, y as u32, color);
-            }
-        }
-    }
-    image.save("font_kit-l8.png").unwrap();
+    GrayImage::from_raw(w as u32, h as u32, pixels).unwrap().save("font_kit-l8.png").unwrap();
 
     let mut base_image = image::open("imgs-000.png")
         .unwrap()
-        .crop_imm(45, 48 - 9, 608, 12)
-        //.crop_imm(0, 0, 200, 100)
+        //.crop_imm(45, 48 - 9, 608, 12)
+        .crop_imm(45, 48 - 9 + 0, 608, 12)
         .into_rgba8();
 
     //image::imageops::overlay(&mut base_image, &image, 0, 0);
     base_image.save("overlay.png").unwrap();
+
+
+    let x_start = 45;
+    let y_start = 48 - 9;
+    let width = 608;
+    let height = 12;
+    let mut reference_image = image::open("imgs-000.png")
+        .unwrap()
+        .crop_imm(x_start, y_start, width, height)
+        .into_luma8();
+
+    let start_pos = Vector2F::new(0., 0.);
+    let alphabet = "> =ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    //let alphabet = "> M";
+    let line = decode_line(&reference_image, &font, 10, size, kern_x, start_pos, alphabet);
+    println!("yo decoded line to `{line}`");
 
 }
 
